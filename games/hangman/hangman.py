91f7ae6abe1e8473e7c78325faea5964162bb7de
Lachlan Paul <lachlanpaul03@gmail.com>
2023-09-18 21:00:32 +0930

    Still in progress, bit more work done

--- games/hangman/hangman.py
+++ games/hangman/hangman.py
@@ -27,11 +27,13 @@ def main():
         print(known_letters)
         if known_letters == letters:
             print("Congrats, you win!")
-        if tries == 11:
+        if tries == 0:
             print("You lose :(")
+            print(f"The word was {word}")
+            break
 
-        guess = input("What's your guess? ")
-        if guess.len() > 1:
+        guess = input("What's your guess? ").lower().strip()
+        if len(guess) > 1:
             print("More than one letter\n")
             continue
 
@@ -40,22 +42,25 @@ def main():
 
 def choose_word(selection):
     if selection == "y":
-        word = random.choice(word_list)
+        word = input("Input your word, make sure nobodies looking! ").lower().strip()
     elif selection == "n":
-        word = input("Input your word, make sure nobodies looking! ")
+        word = random.choice(word_list)
     else:
         raise ValueError
     
     return word
 
 def guess_word(guess):
+    global tries
+
     for i in letters:
-        if i in guess:
-            known_letters.insert(i, guess)
-            known_letters.remove(i)
+        if i == guess:
+            known_letters[i] = guess
         else:
-            tries -= 1
-            print(f"Wrong answer, {tries} tries remaining\n")
+            break
+
+    tries -= 1
+    print(f"Wrong answer, {tries} tries remaining\n")
 
 if __name__ == "__main__":
     main()
